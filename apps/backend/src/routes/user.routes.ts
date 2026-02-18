95c43d822b189e2f13b9a7e8b74e2a090427f11d
Gravyon <rdjmartinez95@gmail.com>
2026-02-18 19:55:29 -0300

    rutas arregladas

--- apps/backend/src/routes/user.routes.ts
+++ apps/backend/src/routes/user.routes.ts
@@ -5,8 +5,8 @@ const router = Router();
 
 router.get("/", UserController.getAll);
 router.get("/:id", UserController.getByUserId);
-router.get("/", UserController.createUser);
-router.get("/:id", UserController.updateUser);
-router.get("/:id", UserController.deleteUser);
+router.post("/", UserController.createUser);
+router.put("/:id", UserController.updateUser); // Podria ser mejor patch para actualizar solo un campo opcionalmente
+router.delete("/:id", UserController.deleteUser);
 
 export default router;
