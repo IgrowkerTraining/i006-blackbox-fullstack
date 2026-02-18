f410541783d793ea778478713cab498b556df3d3
Gravyon <rdjmartinez95@gmail.com>
2026-02-18 20:54:52 -0300

    /api eliminado porque no es necesario

--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/companies", companyRoutes);
-router.use("/api/users", userRoutes);
+router.use("/users", userRoutes);
 
 router.get("/", (_req, res) => {
   res.status(200).json({ status: "Api is working" });
