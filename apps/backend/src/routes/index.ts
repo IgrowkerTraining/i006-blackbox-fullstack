680f9d9440a31bbf001f395aaf1a57f1cd84ce9c
Gravyon <rdjmartinez95@gmail.com>
2026-02-18 20:50:09 -0300

    rutas movidas

--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import authRoutes from "./auth";
 import companyRoutes from "./company";
+import userRoutes from "./user.routes";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/companies", companyRoutes);
+router.use("/api/users", userRoutes);
 
 router.get("/", (_req, res) => {
   res.status(200).json({ status: "Api is working" });
