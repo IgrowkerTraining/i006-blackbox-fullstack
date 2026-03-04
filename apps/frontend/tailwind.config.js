1bf73770c3fd2feca25516c5d6b420134d964f8d
Alejandro <alejodev2022@gmail.com>
2026-03-04 17:16:24 -0500

    feat(tailwind): update Tailwind CSS configuration to include content paths and extend theme colors

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,14 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
-
+      colors: {
+        'blackbox-green': '#0B1001',
+        'blackbox-yellow': '#F2B705',
+        'blackbox-white': '#F8F8F8',
+        'blackbox-blue': '#4E63A8',
+      },
       fontFamily: {
-        lato : [ 'Lato', 'sans-serif']
+        lato: ['Lato', 'sans-serif']
       }
-
-
     },
   },
   plugins: [],
